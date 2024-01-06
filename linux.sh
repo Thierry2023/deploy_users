b106044cffd0c4b2610ac92382e5c1168741bf81
vagrant <nonotthierry@yahoo.fr>
2024-01-06 23:33:23 +0100

    add option to specify secondary group

--- linux.sh
+++ linux.sh
@@ -3,14 +3,14 @@
 if [ $(id -u) -eq 0 ]; then
 	read -p "Enter username : " username
 	read -s -p "Enter very strong password : " password
-
+        read -p "Enter secondary group : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
 		exit 1
 	else
 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p $pass $username
+		useradd -m -g $group -p $pass $username
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
 else
